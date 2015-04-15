9dc55f37ad0c0912468651309713cdbd85136b49
Adam Kawczynski <a.kawczynski@gmail.com>
2015-04-15 12:11:17 +0200

    pyMongo 3.0

--- pyMail/database.py
+++ pyMail/database.py
@@ -1,14 +1,14 @@
-from pymongo import Connection
+from pymongo import MongoClient
 from pyMail.logging import console
 class database:
     db = None
     conn = None
     @staticmethod
-    def instance(): 
+    def instance():
         s = database
         if s.conn is None or s.db is None:
             console.log('I just recconected to the DB')
-            s.conn = Connection()
+            s.conn = MongoClient()
             s.db = s.conn.pymail
-            
+
         return s.db
